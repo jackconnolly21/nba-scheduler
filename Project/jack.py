834e59ad5598c9588fda4ff1d3d5e13a85d73138
jackconnolly21 <jconnolly@college.harvard.edu>
2017-12-07 20:37:30 -0500

    some stuff

--- Project/jack.py
+++ Project/jack.py
@@ -1,6 +1,7 @@
 import util
 from scheduler import Scheduler, Team, Game
 from datetime import date
+from localSearch import hillClimbing, simulatedAnnealing
 import pickle
 import copy
 import random
@@ -9,18 +10,40 @@ from math import e, log
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
+s = Scheduler()
+s.randomStart()
+s.removeTriples()
 
-dates = Counter()
-with open('270.txt', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        d = row[0]
-        year = int(d[:4])
-        month = int(d[5:7])
-        day = int(d[8:])
-        dateObj = date(year, month, day)
-        dates[dateObj] += 1
+HCtraces = []
+SAtraces = []
 
-dates = sorted(dates)
-print dates
+for i in xrange(20):
+    hcsch = deepcopy(s)
+    sasch = deepcopy(s)
+    hcCost = hillClimbing(hcsch, 100)
+    saCost = simulatedAnnealing(sasch, times=100)
+
+    HCtraces.append(hcsch.trace)
+    SAtraces.append(sasch.trace)
+
+hcAvg = []
+saAvg = []
+for i in xrange(len(HCtraces[0])):
+    hc = []
+    sa = []
+    for trial in xrange(len(HCtraces)):
+        hc.append(HCtraces[trial][i])
+        sa.append(SAtraces[trial][i])
+    hcAvg.append(sum(hc)/len(hc))
+    saAvg.append(sum(sa)/len(sa))
+
+pickle.dump(hcAvg, open("hc.txt", 'wb'))
+pickle.dump(saAvg, open("sa.txt", 'wb'))
+
+plt.plot(hcAvg, label="Hill Climbing")
+plt.plot(saAvg, label="Simulated Annealing")
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+       ncol=2, mode="expand", borderaxespad=0.)
+plt.show()
